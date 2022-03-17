81547c7c0a11932b4cf4865f58d007ec31dd0d64
daehwan0307 <eoghks960307@naver.com>
2022-03-17 20:44:47 +0900

    chatting card 수정

--- winwin/components/ChattingCard.js
+++ winwin/components/ChattingCard.js
@@ -10,12 +10,23 @@ const ChattingCard =()=>{
             <div>
                 <Link href="/main" passHref><Image  src="/favicon.ico" width={40} height={40} ></Image></Link>
             </div>
-            <div className="font-bold">
-                아리랑님, 00님이 채팅을 걸어왔어요. 확인해보세요!
+                <div >
+                    <div className="font-bold">
+                        아리랑
+                    </div>
+                    <div className="font-">
+                        넹
+                    </div>
+                </div>
+                <div className="pl-12 text-gray-400 text-sm">3분전</div>
+                <div className='flex float-right '>
+                    <div>코딩</div>
+                    <div>교환</div>
+                    <div>영어</div>
+                </div>
             </div>
-            </div>
-            <div className="pl-12 text-gray-400 text-sm">3분전</div>
-     
+           
+            <hr></hr>
         </div>
 
     );
