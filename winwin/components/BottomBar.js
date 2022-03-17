ad709135146bc6c40682a2cd8c40f8f19590357f
daehwan0307 <eoghks960307@naver.com>
2022-03-17 18:57:23 +0900

    chatting page

--- winwin/components/BottomBar.js
+++ winwin/components/BottomBar.js
@@ -1,24 +1,25 @@
+import Link from "next/link";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAlignJustify, faBell, faComment, faLocation, faHome, faPerson } from "@fortawesome/free-solid-svg-icons";
 
 const BomttomBar=()=>{
     return(
 
-        <div className="flex justify-between  sm: container">
+        <footer className="flex justify-between  sm: container">
             
             <div>
                 <div><FontAwesomeIcon icon={faHome} size="2x" color="black"/><br/><span className="font-light text-xs">홈</span></div>
             </div>
                 
             <div>
-                <div><FontAwesomeIcon icon={faComment} size="2x" color="black"/><br/><span className="font-light text-xs">채팅</span></div>
+                <Link href="/chatting_list"><div><FontAwesomeIcon icon={faComment} size="2x" color="black"/><br/><span className="font-light text-xs">채팅</span></div></Link>
             </div>
 
             <div>
                 <div><FontAwesomeIcon icon={faPerson} size="2x" color="black"/><br/><span className="font-light text-xs" >마이페이지</span></div>
             </div>
               
-        </div>
+        </footer>
 
     );
 }
---
+++ winwin/components/ChattingCard.js
@@ -0,0 +1,23 @@
+import Link from 'next/link'
+import Image from 'next/image'
+
+const ChattingCard =()=>{
+
+    return(
+
+            <div className="sm:container  ">
+            <div className="flex space-x-2 pt-2">
+            <div>
+                <Link href="/main" passHref><Image  src="/favicon.ico" width={40} height={40} ></Image></Link>
+            </div>
+            <div className="font-bold">
+                아리랑님, 00님이 채팅을 걸어왔어요. 확인해보세요!
+            </div>
+            </div>
+            <div className="pl-12 text-gray-400 text-sm">3분전</div>
+     
+        </div>
+
+    );
+}
+export default ChattingCard;
